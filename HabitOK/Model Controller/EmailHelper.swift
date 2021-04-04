b4940a8317e41b09992c5359812be898cc47c4eb
Ярослав Шерстюк <sherstyuk.ya.n@gmail.com>
2021-04-04 18:45:40 +0300

    Add Settings new func

---
+++ HabitOK/Model Controller/EmailHelper.swift
@@ -0,0 +1,41 @@
+//
+//  EmailHelper.swift
+//  HabitOK
+//
+//  Created by Ярослав Шерстюк on 04.04.2021.
+//
+
+import Foundation
+import MessageUI
+
+class EmailHelper: NSObject, MFMailComposeViewControllerDelegate {
+    public static let shared = EmailHelper()
+    private override init() {
+        //
+    }
+    
+    func sendEmail(subject:String, body:String, to:String){
+        if !MFMailComposeViewController.canSendMail() {
+            // Utilities.showErrorBanner(title: "No mail account found", subtitle: "Please setup a mail account")
+            return //EXIT
+        }
+        
+        let picker = MFMailComposeViewController()
+        
+        picker.setSubject(subject)
+        picker.setMessageBody(body, isHTML: true)
+        picker.setToRecipients([to])
+        picker.mailComposeDelegate = self
+        
+        EmailHelper.getRootViewController()?.present(picker, animated: true, completion: nil)
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        EmailHelper.getRootViewController()?.dismiss(animated: true, completion: nil)
+    }
+    
+    static func getRootViewController() -> UIViewController? {
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.window?.rootViewController
+    }
+}
+
--- HabitOK/SettingsView.swift
+++ HabitOK/SettingsView.swift
@@ -6,13 +6,18 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct SettingsView: View {
     
     var body: some View {
+        
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+        let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as! String
+        
         NavigationView {
             Form {
-                Section(header: Text("Management")){
+                Section(header: Text("Management")) {
                     
                     NavigationLink(destination: ListAreasView()) {
                         Text("Areas management")
@@ -22,10 +27,72 @@ struct SettingsView: View {
                         Text("Habits management")
                     }
                 }
+                
+                Section(header: Contact()) {
+                    
+                    Link("Twitter", destination: URL(string: "https://twitter.com/habit_ok")!)
+                    
+                    Button(action: {
+                        EmailHelper.shared.sendEmail(subject: "HabitOK",
+                                                     body: "Version: \(appVersion)",
+                                                     to: "habitok.app@gmail.com")
+                    }) {
+                        Text("Email")
+                    }
+                }
+                
+                Section(header: About()) {
+                    // MARK: - Кнопка оценить приложение
+                    Button(action: {
+                        if let windowScene = UIApplication.shared.windows.first?.windowScene { SKStoreReviewController.requestReview(in: windowScene) }
+                    }) {
+                        Text("Rate app")
+                    }
+                    
+                    // MARK: - Кнопка рассказать другу
+                    Button(action: {
+                        tellFriend()
+                    }) {
+                        Text("Share HabitOK")
+                    }
+                    
+                    HStack {
+                        Text("Version App")
+                        Spacer()
+                        Text("\(appVersion) (\(buildNumber))")
+                    }
+                }
             }
             
             .navigationTitle(Text("Settings"))
             
+        }
+    }
+    
+    // MARK: - Рассказать другу
+    func tellFriend() {
+        
+        let text = URL(string: "https://apps.apple.com/ua/app/%D1%82%D0%BE%D0%BF%D0%B8%D0%BA/id1552880676")
+        let data = [text]
+        let av = UIActivityViewController(activityItems: data as [Any], applicationActivities: nil)
+        UIApplication.shared.windows.first?.rootViewController?.present(av, animated: true, completion: nil)
+    }
+}
+
+struct Contact: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "text.bubble")
+            Text("Contact")
+        }
+    }
+}
+
+struct About: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "info.circle")
+            Text("About")
         }
     }
 }
